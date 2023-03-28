43d89d9e0061a5d36a0f0cec07f4a9ac474c60ce
aishtiaq <awshaf_ishtiaque@sfu.ca>
2023-03-27 19:03:27 -0700

    added endpoint for getall, add, delete note

--- api/routes/testDB.js
+++ api/routes/testDB.js
@@ -1,26 +1,80 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
-// Variable to be sent to Frontend with Database status
+const Blog = require("./blog");
+
 let databaseConnection = "Waiting for Database response...";
 
-router.get("/", function(req, res, next) {
-    res.send(databaseConnection);
-});
-
-const uri = 'mongodb+srv://new-user-01:dbpass123@cluster0.hxneqft.mongodb.net/?retryWrites=true&w=majority'
-
 // Connecting to MongoDB
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = process.env.MONGO_URI;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
     databaseConnection = "Connected to Database";
   })
-  .catch(err => {
-    console.log('yooo MongoDB connection error:', err);
+  .catch((err) => {
+    console.log("yooo MongoDB connection error:", err);
     databaseConnection = "Error connecting to Database";
   });
 
 module.exports = router;
+
+router.get("/", function (req, res, next) {
+  res.send(databaseConnection);
+});
+
+
+//CURD Functionalities:
+router.post("/addnote", function (req, res, next) {
+  const { title, content } = req.body;
+  console.log("title:", title);
+  console.log("content:", content);
+  if (title && content) {
+    const blog = new Blog({
+      title: title,
+      content: content,
+    });
+    blog
+      .save()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send("err saving data");
+      });
+  }
+});
+
+router.get("/allnotes", function (req, res, next) {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("err saving data");
+    });
+});
+
+router.post("/deletenote/:id", function (req, res, next) {
+  const id = req.params.id
+  console.log('id:',id);
+  try{
+    Blog.findByIdAndDelete(id)
+    .then((result) => {
+      console.log('delete id:', id);
+      res.send('deleted id '+ id);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("CANT find with id:" + id);
+    });
+  }catch (err){
+    console.log(err)
+  }
+
+});
